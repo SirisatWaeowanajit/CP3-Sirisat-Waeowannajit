5a3392cb3b1b4117749ba222a080494ea89c090b
ThaitanDev <67495233+ThaitanDev@users.noreply.github.com>
2020-06-30 09:22:22 +0700

    Added variable

---
+++ Exercise4_Sirisat_S.py
@@ -0,0 +1,12 @@
+F = float(80)
+G = float(75)
+I = float(90)
+N = float(84)
+
+
+print("\n--- Your Score ---\n")
+
+print("Foundation English               :",F)
+print("General Bussiness                :",G)
+print("Introduction to Computer Systems :",I)
+print("Computer Programming             :",N)
